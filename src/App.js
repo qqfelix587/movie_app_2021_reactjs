0ed278f08660d3b8eddd561f6f1ca7bc9577f49d
qqfelix <qqfelix@naver.com>
2021-12-25 18:33:53 +0900

    lec1 state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from "prop-types";
+import React from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component{
+  state = {
+    count: 0
+  };
+  add=()=>{
+    //this.setState({count:this.state.count+ 1})
+
+    this.setState(current => ({count:current.count+ 1}))
+  }
+  remove=()=>{
+    this.setState(current => ({count:current.count - 1}))
+  }
+  render(){
+    return <div>
+      
+      <h1>The number is: {this.state.count}</h1>
+      <button onClick={this.add}>plus</button>
+      <button onClick={this.remove}>minus</button>
+      </div>
+
+    
+  }
 }
 
 export default App;
